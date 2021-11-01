7a855a4c64f10863b6ba14b5e8f85121fd4e3830
George Okumu <gokumu12@gmail.com>
2021-11-01 22:49:32 +0300

    making betty happy

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "sort.h"
+/**
+ * bubble_sort - for sorting values in an array
+ * @array: array to be swaped
+ * @size: unsigned size
+ */
 
 void bubble_sort(int *array, size_t size)
 {
@@ -8,22 +13,22 @@ unsigned int i;
 unsigned  int j;
 int temp = 0;
 
-if(array == NULL || size == 0)
-        return;
+if (array == NULL || size == 0)
+return;
 
-for (i = 0; i < size -1; i++)
+for (i = 0; i < size - 1; i++)
 {
-    for(j = 0; j < size - 1; j++)
-    {
-        if(array[i] > array[j + 1])
-        {
-          /*Swap the arrays @ each array index*/ 
-          temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-          /*Print the array*/
-          print_array(array, size);
-        }
-    }
+for (j = 0; j < size - 1; j++)
+{
+if (array[i] > array[j + 1])
+{
+/*Swap the arrays @ each array index*/
+temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+/*Print the array*/
+print_array(array, size);
+}
+}
 }
 }
